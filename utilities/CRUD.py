d684b511b388061edb54340d9ce8479742c176a0
SUSHANT SUMAN <81477161+sush-5355@users.noreply.github.com>
2023-08-27 23:46:56 +0530

    Update CRUD.py

--- utilities/CRUD.py
+++ utilities/CRUD.py
@@ -1,14 +1,8 @@
-from utilities.configuration import es
-from datetime import datetime,timezone
+from utilities.elastic_config import *
+from utilities.date_conversion import convert_date
 
-def convert_date():
-    date_convert =  datetime.now(timezone.utc).astimezone()
-    converted_date = str(date_convert).split('.')[0]
-    d,t = converted_date.split(' ')[0],converted_date.split(' ')[1]
-    converted_date = d + 'T' + t
-    return converted_date
 
-async def delete_index(index_name):
+def delete_index(index_name):
     index_exists = es.indices.exists(index=index_name)
     if index_exists:
         es.indices.delete(index=index_name)
@@ -16,11 +10,12 @@ async def delete_index(index_name):
     return {'status': False, 'msg': f'Index - {index_name} already deleted or does not exists'}
 
 
-async def update_by_id(index_name, id, doc):
+def update_by_id(index_name, id, doc):
     index_exists = es.indices.exists(index=index_name)
     if index_exists:
         try:
-            doc.update({'updated_at':convert_date()})
+            # doc.update({'updated_at':convert_date()})
+            # del doc['_id']
             return es.update(index=index_name, id=id, body={"doc": doc})
         except ModuleNotFoundError:
             return {'status': False, 'msg': f'id = {id} does not exists'}
@@ -28,7 +23,7 @@ async def update_by_id(index_name, id, doc):
         return {'status': False, 'msg': f'Index = {index_name} does not exists'}
 
 
-async def delete(index_name, id):
+def delete(index_name, id):
     index_exists = es.indices.exists(index=index_name)
     if index_exists:
         es.delete(index=index_name, id=id)
@@ -36,7 +31,7 @@ async def delete(index_name, id):
     return {'status': False, 'msg': f'Index - {index_name} already deleted or does not exists'}
 
 
-async def get_by_id(index_name, id):
+def get_by_id(index_name, id):
     index_exists = es.indices.exists(index=index_name)
     if index_exists:
         try:
@@ -47,35 +42,64 @@ async def get_by_id(index_name, id):
     else:
         return {'status': False, 'msg': f'Index = {index_name} does not exists'}
 
+def get_by_query_agg(index_name, query):
+    result = es.search(index=index_name, body=query)
+    return result
 
-async def get_by_query(index_name, query):
+def get_by_query(index_name, query):
     result = es.search(index=index_name, body=query)
     result = result['hits']['hits']
     final_list = []
     for res in result:
+        res['_source'].update({'_id':res['_id']})
         final_list.append(res['_source'])
     return final_list
 
 
-async def get_all(index_name):
-    result = es.search(index=index_name, body={"query": {"match_all": {}}})
+def get_all(index_name,size=None):
+    result = es.search(index=index_name, body={"size":size,"query": {"match_all": {}}})
     result = result['hits']['hits']
+    count =len(result)
     final_list = []
     for res in result:
+        res['_source'].update({'_id':res['_id']})
         final_list.append(res['_source'])
-    return final_list
+        
+    return dict(data=final_list,count=count)
 
 
-async def create(index_name, mapping):
+def create(index_name, mapping:dict):
     index_exists = es.indices.exists(index=index_name)
     if not index_exists:
         es.indices.create(index=index_name)
     mapping.update({'created_at':convert_date(),'updated_at':convert_date()})
     response = es.index(index=index_name, body=mapping)
-    response_get_by_id = await get_by_id(index_name=index_name, id=response['_id'])
+    response_get_by_id =   get_by_id(index_name=index_name, id=response['_id'])
     try:
         response_get_by_id.update({'id': response['_id']})
-        response_update = await update_by_id(index_name=index_name, id=response['_id'], doc=response_get_by_id)
-        return await get_by_id(index_name=index_name, id=response_update['_id'])
+        response_update =   update_by_id(index_name=index_name, id=response['_id'], doc=response_get_by_id)
+        return   get_by_id(index_name=index_name, id=response_update['_id'])
     except Exception as e:
         return e
+
+def get_all_scroll(index_name, batch_size):
+    # Initialize the scroll API
+    scroll = '2m' # Keep the search context alive for 2 minutes
+    search_body = {"query": {"match_all": {}}}
+    search_result = es.search(index=index_name, body=search_body, scroll=scroll, size=batch_size)
+
+    # Get the total number of hits and initialize the results list
+    total_hits = search_result['hits']['total']['value']
+    results = []
+
+    # Keep scrolling until all documents have been retrieved
+    while len(search_result['hits']['hits']) > 0:
+        # Append the current batch of results to the final list
+        results += [hit['_source'] for hit in search_result['hits']['hits']]
+
+        # Use the scroll ID to retrieve the next batch of results
+        scroll_id = search_result['_scroll_id']
+        search_result = es.scroll(scroll_id=scroll_id, scroll=scroll)
+
+    # Return the final list of results
+    return results
